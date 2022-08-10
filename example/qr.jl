e23d99c83a8d84d87efa651aa53b87735a141c89
Sascha Klawohn <sascha.klawohn@warwick.ac.uk>
2022-08-10 15:33:10 +0200

    Add QR example

---
+++ example/qr.jl
@@ -0,0 +1,182 @@
+import Random
+using ArgParse
+using ScaLAPACK
+
+function parse_commandline()
+    s = ArgParseSettings()
+    s.description = """
+        Solve A(m,n) X(n,k) = B(m,k) for X with m ≫ n > 0 using {p,1} process grid
+
+        We create the random numbers for A and B element-wise for reproducibility
+        and locally to keep the memory peak small.
+        """
+
+    @add_arg_table s begin
+        "--nrows", "-R"
+            help = "Number of rows for matrices A and B"
+            arg_type = Int
+            default = 16
+        "--ncols", "-C"
+            help = "Number of columns for matrix A"
+            arg_type = Int
+            default = 4
+        "--nrhs", "-N"
+            help = "Number of columns for matrix B"
+            arg_type = Int
+            default = 1
+
+        "--nbrows", "-r"
+            help = "Row block size for matrix A and B"
+            arg_type = Int
+            default = 0
+        "--nbcols", "-c"
+            help = "Col block size for matrix A"
+            arg_type = Int
+            default = 100
+        "--nbrhs", "-n"
+            help = "Col block size for matrix B"
+            arg_type = Int
+            default = 0
+
+        "--print-matrices"
+            help = "Whether to print A, B, X"
+            action = :store_true
+        "--afile", "-A"
+            help = "File to write A into"
+            default = "A"
+        "--bfile", "-B"
+            help = "File to write B into"
+            default = "B"
+        "--xfile", "-X"
+            help = "File to write X into"
+            default = "X"
+
+        "--allout"
+            help = "All processes print"
+            action = :store_true
+        "--order"
+            help = "Whether matrices are row-major (R) or col-major (C) order"
+            range_tester = (x -> x == 'R' || x == 'C')
+            default = 'R'
+    end
+
+    return parse_args(s)
+end
+
+function prandom_each(mg, ng, mb, nb, ml, nl, mypnum, nprocs; seed=-1)
+    @assert mb * nprocs >= mg  # simpler locating
+
+    A = zeros(ml, nl)
+    v = zeros(1)
+
+    offset = mypnum * mb
+
+    seed >= 0 && Random.seed!(seed)
+    for j in 1:ng, i in 1:mg
+        Random.rand!(v)
+        li = i - offset
+        if 0 < li <= ml
+            A[li,j] = v[1]
+        end
+    end
+
+    return A
+end
+
+function pmatrix_to_file(matrix, prefix, mypnum)
+    open("$prefix.$mypnum", "w") do f
+        println(f, matrix)
+    end
+end
+
+function solve_qr!(A::Matrix{Float64}, descA::Vector{Cint},
+        B::Matrix{Float64}, descB::Vector{Cint})
+    mg = descA[3]
+    ng = descA[4]
+    @assert mg == descB[3]
+    kg = descB[4]
+
+    tauA = zeros(min(mg, ng))
+    work = zeros(1)
+    info = Ref{Cint}(-1)
+
+    pdgeqrf!(mg, ng, A, 1, 1, descA, tauA, work, -1, info)
+    @assert info[] == 0
+    lwork = Cint(work[1])
+    work = zeros(lwork)
+    pdgeqrf!(mg, ng, A, 1, 1, descA, tauA, work, lwork, info)
+    @assert info[] == 0
+
+    pdormqr!('L', 'T', mg, kg, ng, A, 1, 1, descA,
+        tauA, B, 1, 1, descB, work, -1, info)
+    @assert info[] == 0
+    lwork = Cint(work[1])
+    if lwork > length(work)
+        work = zeros(lwork)
+    end
+    pdormqr!('L', 'T', mg, kg, ng, A, 1, 1, descA,
+        tauA, B, 1, 1, descB, work, lwork, info)
+    @assert info[] == 0
+
+    # cheats trtrs that mb == nb without increasing memory for geqrf, ormqr; asserts q == 1
+    nb = descA[6]
+    descA[6] = descA[5]
+    pdtrtrs!('U', 'N', 'N', ng, kg, A, 1, 1, descA, B, 1, 1, descB, info)
+    @assert info[] == 0
+    descA[6] = nb
+
+    return B, descB
+end
+
+function main()
+    args = parse_commandline()
+
+    mypnum, nprocs = blacs_pinfo()
+    args["allout"] && mypnum != 0 && redirect_stdout(devnull)
+
+    ictxt = blacs_get(0, 0)
+    blacs_gridinit(ictxt, args["order"], nprocs, 1)
+    nprow, npcol, myrow, mycol = blacs_gridinfo(ictxt)
+
+    mg = args["nrows"]
+    if mg < 1
+        mg = nprocs
+    end
+    ng = args["ncols"]
+    kg = args["nrhs"]
+
+    mb = args["nbrows"]
+    if mb < 1
+        mb = ceil(Integer, mg / nprow)
+    end
+    nb = args["nbcols"]
+    if nb < 1
+        nb = ceil(Integer, ng / npcol)
+    end
+    kb = args["nbrhs"]
+    if kb < 1
+        kb = ceil(Integer, kg / npcol)
+    end
+    ml = numroc(mg, mb, myrow, 0, nprow)
+    nl = numroc(ng, nb, mycol, 0, npcol)
+    kl = numroc(kg, kb, mycol, 0, npcol)
+
+    mg < ng && error("X will not fit into B: mg = $mg < $ng = ng")
+
+    do_files = args["print-matrices"]
+
+    A = prandom_each(mg, ng, mb, nb, ml, nl, mypnum, nprocs, seed=0)
+    B = prandom_each(mg, kg, mb, kb, ml, kl, mypnum, nprocs, seed=1)
+    descA = descinit(mg, ng, mb, nb, 0, 0, ictxt, ml)
+    descB = descinit(mg, kg, mb, kb, 0, 0, ictxt, ml)
+    do_files && pmatrix_to_file(A, "A", mypnum)
+    do_files && pmatrix_to_file(B, "B", mypnum)
+
+    X, descX = solve_qr!(A, descA, B, descB)
+    do_files && pmatrix_to_file(X, "X", mypnum)
+
+    blacs_gridexit(ictxt)
+    blacs_exit(0)
+end
+
+main()
