2388737aaedeb92cd5316e2015ddc20db2529389
Sascha Klawohn <sascha.klawohn@warwick.ac.uk>
2022-08-11 18:13:01 +0200

    Revert "add redist copy"
    This reverts commit 3713f6bb19ed97ffcc0443439a485843ee1982b6.

--- test/test_redist.jl
+++ test/test_redist.jl
@@ -2,33 +2,6 @@ include("common.jl")
 
 @testset "REDIST" begin
 
-function test_copy()
-@testset "[4x3(4x3)] {2X1}->{2X1} [4x3(4x3)<1,0>]" begin
-    ictxt = sl_init(2, 1)
-    mypnum >= 2 && return
-
-    if mypnum == 0
-        A = [1.0 5.0 9.0 ; 2.0 6.0 10.0 ; 3.0 7.0 11.0 ; 4.0 8.0 12.0]
-        B = zeros(4, 3)
-    else
-        A = [0.0;;]
-        B = [0.0;;]
-    end
-    descA = descinit(4, 3, 4, 3, 0, 0, ictxt, 4)
-    descB = descinit(4, 3, 4, 3, 0, 0, ictxt, 4)
-
-    # pdgemr2d(m, n, A, ia, ja, desc_A, B, ib, jb, desc_B, gcontext)
-    pdgemr2d!(4, 3, A, 1, 1, descA, B, 1, 1, descB, ictxt)
-    if mypnum == 0
-        @test B == [1.0 5.0 9.0 ; 2.0 6.0 10.0 ; 3.0 7.0 11.0 ; 4.0 8.0 12.0]
-    else
-        @test B == [0.0;;]
-    end
-
-    blacs_gridexit(ictxt)
-end
-end
-
 function test_change_blocksize()
 @testset "[4x3(4x3)] {2X1}->{2X1} [3x3(2x3)<1,0>]" begin
     ictxt = sl_init(2, 1)
@@ -87,7 +60,6 @@ end
 end
 
 @testset "pdgemr2d!" begin
-    test_copy()
     test_change_blocksize()
     test_change_context()
 end
