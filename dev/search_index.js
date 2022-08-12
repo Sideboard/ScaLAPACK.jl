25bdd59da8c4ae116984b0e948c3359c2f20c061
Documenter.jl <documenter@juliadocs.github.io>
2022-08-12 09:13:34 +0000

    build based on d7732b0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ScaLAPACK","category":"page"},{"location":"#ScaLAPACK","page":"Home","title":"ScaLAPACK","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ScaLAPACK.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ScaLAPACK]","category":"page"}]
+}
